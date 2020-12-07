dabe51346e336f1ca785f9060b0f7b2c7e5c0042
mengMia <mengya@ahu.edu.cn>
2020-12-08 07:39:32 +0800

    9.4 app控件定位-雪球app搜索阿里巴巴

---
+++ PythonCode/App9/test_app4.py
@@ -0,0 +1,44 @@
+from time import sleep
+
+import pytest
+from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
+
+class TestFind():
+    def setup(self):
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '6.0'
+        desired_caps['deviceName'] = 'emulator-5554'
+        desired_caps['appPackage'] = 'com.xueqiu.android'
+        desired_caps['appActivity'] = '.view.WelcomeActivityAlias'
+        desired_caps['noReset'] = True
+        # desired_caps['dontStopAppOnReset'] = True
+        desired_caps['skipDeviceInitialization'] = True
+        desired_caps['unicodeKeyBoard'] = 'true' # 输入中文搜索词的时候需要加这个，但是貌似不加也不会有问题
+        desired_caps['resetKeyBoard'] = 'true'
+        self.driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
+        self.driver.implicitly_wait(5)
+
+    def teardown(self):
+        sleep(3)
+        self.driver.quit()
+
+    def test_search(self):
+        """
+        1.打开雪球app
+        2.点击搜索输入框
+        3.向搜索输入框输入”阿里巴巴“
+        4.在搜索结果里面选择“阿里巴巴”，点击
+        5.获取这只香港阿里巴巴的股价，并判断价格>200
+        :return:
+        """
+        self.driver.find_element_by_id("com.xueqiu.android:id/tv_search").click()
+        self.driver.find_element_by_id("com.xueqiu.android:id/search_input_text").send_keys("阿里巴巴")
+        self.driver.find_element(MobileBy.XPATH, '//*[@resource-id="com.xueqiu.android:id/name" and @text="阿里巴巴"]').click()
+        current_price = float(self.driver.find_element(MobileBy.ID, 'com.xueqiu.android:id/current_price').text)
+        print(current_price)
+        assert current_price > 200
+
+    if __name__ == '__main__':
+        pytest.main()
