a6c5dfd90a572cb2c42af8970bf2846da55f8fd9
mengMia <mengya@ahu.edu.cn>
2021-01-01 21:57:28 +0800

    11.5 企业微信app实战5-添加联系人PO模式-添加成员方式选择页，封装driver和find_element方法

--- PythonCode/App11/page/member_invite_menu_page.py
+++ PythonCode/App11/page/member_invite_menu_page.py
@@ -1,26 +1,27 @@
 from appium.webdriver.common.mobileby import MobileBy
 
 # from PythonCode.App11.page.contactadd_page import ContactAddPage
+from PythonCode.App11.page.base_page import BasePage
 
 """
 邀请（添加）联系人页面
 """
 
-class MemberInviteMenuPage:
-    def __init__(self, driver):
-        """
-        定义一个方法接收driver
-        """
-        self.driver = driver
+class MemberInviteMenuPage(BasePage):
+    # def __init__(self, driver):
+    #     """
+    #     定义一个方法接收driver
+    #     """
+    #     self.driver = driver
 
     def add_member_manual(self):
         """
         点击“手动输入添加”
         """
-        self.driver.find_element(MobileBy.XPATH, '//*[@text="手动输入添加"]').click()
+        self.find(MobileBy.XPATH, '//*[@text="手动输入添加"]').click()
         from PythonCode.App11.page.contactadd_page import ContactAddPage
         return ContactAddPage(self.driver)
 
     def get_toast(self):
-        text_toast = self.driver.find_element(MobileBy.XPATH, '//*[@class="android.widget.Toast"]').text   # 这里find_element方法会调用隐式等待，每隔0.5s去查找toast
+        text_toast = self.get_toast_text()
         return text_toast
