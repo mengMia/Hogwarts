c6e6867c7397b57b8d7a5c058ab181e71bbd93c9
mengMia <mengya@ahu.edu.cn>
2021-01-01 21:56:57 +0800

    11.5 企业微信app实战5-添加联系人PO模式-企业微信主页，封装driver和find_element方法

--- PythonCode/App11/page/main_page.py
+++ PythonCode/App11/page/main_page.py
@@ -1,15 +1,17 @@
 from appium.webdriver.common.mobileby import MobileBy
 
 from PythonCode.App11.page.addresslist_page import AddressListPage
+from PythonCode.App11.page.base_page import BasePage
 
 
-class MainPage:
-    def __init__(self, driver):
-        """
-        定义一个方法接收driver
-        """
-        self.driver = driver
-
+class MainPage(BasePage):
+    # def __init__(self, driver):
+    #     """
+    #     定义一个方法接收driver
+    #     """
+    #     self.driver = driver
+    # 把定位参数定义成一个类变量传入方法中
+    address_element = (MobileBy.XPATH, '//*[@text="通讯录"]')
     def goto_message(self):
         """
         进入到消息页
@@ -22,5 +24,6 @@ class MainPage:
         点击通讯录tab，进入到通讯录页面
         :return:
         """
-        self.driver.find_element(MobileBy.XPATH, '//*[@text="通讯录"]').click()
+        # self.find(*self.address_element).click()
+        self.find_and_click(*self.address_element)
         return AddressListPage(self.driver)
