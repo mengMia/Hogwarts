63c658a62497322013a00d0fd5b4dadc78925a0e
mengMia <mengya@ahu.edu.cn>
2021-01-01 17:15:59 +0800

    11.4 企业微信移动app实战3-添加联系人

--- PythonCode/App11/test_app4.py
+++ PythonCode/App11/test_app4.py
@@ -35,17 +35,23 @@ class TestContact():
         name = "test4"
         gender = "男"
         phonenum = "13500000004"
+        # 点击通讯录tab按钮
         self.driver.find_element(MobileBy.XPATH, '//*[@text="通讯录"]').click()
+        # 滚动查找“添加成员”按钮
         self.driver.find_element_by_android_uiautomator('new UiScrollable(new UiSelector().'
                                                         'scrollable(true).instance(0)).'
                                                         'scrollIntoView(new UiSelector().text("添加成员").'
                                                         'instance(0))').click()
+        # 点击”手动输入添加“
         self.driver.find_element(MobileBy.XPATH, '//*[@text="手动输入添加"]').click()
+
         # 联系人表单
 
         # 输入姓名
         self.driver.find_element(MobileBy.XPATH, '//*[contains(@text, "姓名")]/../android.widget.EditText').send_keys(name)
         # 或者通过：//*[contains(@text, '姓名')]/../*[@text="必填"]来定位这个姓名的输入框
+
+        # 选择性别
         self.driver.find_element(MobileBy.XPATH, '// *[contains( @ text, "性别")] /..// *[ @ text = "男"]').click()
         if gender == "男":
             # 这里需要加个显示等待，因为从上面一步定位的男，切换到下面再定位男，会有报错，
@@ -53,11 +59,16 @@ class TestContact():
             self.driver.find_element(MobileBy.XPATH, '//*[@text="男"]').click()
         else:
             self.driver.find_element(MobileBy.XPATH, '//*[@text="女"]').click()
+
+        # 输入手机号
         self.driver.find_element(MobileBy.XPATH, '//*[contains(@text, "手机") and contains(@class, "TextView")]/..//*[@text="手机号"]').send_keys(phonenum)
+
+        # 点击保存
         self.driver.find_element(MobileBy.XPATH, '//*[@text="保存"]').click()
 
         # sleep(2)
         # print(self.driver.page_source) 需要等待2s才能抓取到toast
+        # 添加断言
         text_toast = self.driver.find_element(MobileBy.XPATH, '//*[@class="android.widget.Toast"]').text   # 这里find_element方法会调用隐式等待，每隔0.5s去查找toast
         assert '添加成功' == text_toast
 
