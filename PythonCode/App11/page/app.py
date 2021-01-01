1c2a61e982cc2db8edc2fe0470efe55b22cd0526
mengMia <mengya@ahu.edu.cn>
2021-01-01 21:55:16 +0800

    11.5 企业微信app实战5-添加联系人PO模式-app模块，封装caps和复用driver

--- PythonCode/App11/page/app.py
+++ PythonCode/App11/page/app.py
@@ -4,34 +4,48 @@ app.py 模块，存放app相关的一些操作。
 """
 from time import sleep
 
+import yaml
 from appium import webdriver
 
+from PythonCode.App11.page.base_page import BasePage
 from PythonCode.App11.page.main_page import MainPage
 
 
-class App:
+with open('../datas/caps.yml') as f:
+    myconfig = yaml.safe_load(f)
+    caps = myconfig['desirecaps']
+    ip = myconfig['server']['ip']
+    port = myconfig['server']['port']
+
+class App(BasePage):
     def start(self):
-        # 启动app
-        desired_caps = {}
-        desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '6.0'
-        desired_caps['deviceName'] = 'emulator-5554'
-        desired_caps['appPackage'] = 'com.tencent.wework'
-        desired_caps['appActivity'] = '.launch.LaunchSplashActivity'
-        desired_caps['noReset'] = True  # 这里的noreset设置为true，重启app的时候就不会清除登录信息
-        desired_caps['skipDeviceInitialization'] = True
-        desired_caps['unicodeKeyBoard'] = 'true'
-        desired_caps['resetKeyBoard'] = 'true'
-        # desired_caps['settings[waitForIdleTimeout]'] = 0
-        desired_caps['skipServerInstallation'] = True
-        # 使用appium自动启动指定的模拟器
-        self.driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
-        self.driver.implicitly_wait(5)
+        # app里面每次调用start方法的时候还会实例化一个driver，要想复用driver的话，需要加一个判断（因为给的默认值是None），
+        # 在test_contact里面第一次调用start方法的时候，driver拥有了一个值，但是第二条测试用例再去调用start，还是会重新实例化一个driver，所以可以判断一下，如果已经有值了，就不进行实例化
+        if self.driver == None:
+            # 启动app
+            # desired_caps = {}
+            # desired_caps['platformName'] = 'Android'
+            # desired_caps['platformVersion'] = '6.0'
+            # desired_caps['deviceName'] = 'emulator-5554'
+            # desired_caps['appPackage'] = 'com.tencent.wework'
+            # desired_caps['appActivity'] = '.launch.LaunchSplashActivity'
+            # desired_caps['noReset'] = True  # 这里的noreset设置为true，重启app的时候就不会清除登录信息
+            # desired_caps['skipDeviceInitialization'] = True
+            # desired_caps['unicodeKeyBoard'] = 'true'
+            # desired_caps['resetKeyBoard'] = 'true'
+            # desired_caps['skipServerInstallation'] = True
+            self.driver = webdriver.Remote(f"http://{ip}:{port}/wd/hub", caps)
+            self.driver.implicitly_wait(5)
+        else:
+            # 如果已经存在了，直接启动app,复用的逻辑
+            # 这个launch_app是直接使用第一次实例化driver的时候定义的appPackage和appActivity
+            self.driver.launch_app()
         return self  #这句很关键，调用start之后，又返回到app了，所以可以链式调用：self.app = App()；# self.main = self.app.start().goto_main()
 
     def restart(self):
         # 重启app
-        pass
+        self.driver.close_app()
+        self.driver.launch_app()
 
     def stop(self):
         # 停止app
