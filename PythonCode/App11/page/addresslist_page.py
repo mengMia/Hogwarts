77f3f12e381185b69680cf04199653d863e78466
mengMia <mengya@ahu.edu.cn>
2021-01-01 21:54:42 +0800

    11.5 企业微信app实战5-添加联系人PO模式-通讯录列表页，封装driver和滚动查找

--- PythonCode/App11/page/addresslist_page.py
+++ PythonCode/App11/page/addresslist_page.py
@@ -2,24 +2,22 @@
 """
 通讯录界面
 """
+from PythonCode.App11.page.base_page import BasePage
 from PythonCode.App11.page.member_invite_menu_page import MemberInviteMenuPage
 
 
-class AddressListPage:
-    def __init__(self, driver):
-        """
-        定义一个方法接收driver
-        """
-        self.driver = driver
+class AddressListPage(BasePage):
+    # def __init__(self, driver):
+    #     """
+    #     定义一个方法接收driver
+    #     """
+    #     self.driver = driver
 
     def click_addmember(self):
         """
         点击添加成员按钮
         """
         # 滚动查找”添加成员“按钮
-        self.driver.find_element_by_android_uiautomator('new UiScrollable(new UiSelector().'
-                                                        'scrollable(true).instance(0)).'
-                                                        'scrollIntoView(new UiSelector().text("添加成员").'
-                                                        'instance(0))').click()
+        self.find_by_scroll("添加成员").click()
 
         return MemberInviteMenuPage(self.driver)
