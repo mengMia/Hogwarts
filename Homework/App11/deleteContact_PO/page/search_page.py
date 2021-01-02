165b2bbca7d23cde514e5c043412a21e87033019
mengMia <mengya@ahu.edu.cn>
2021-01-02 15:43:03 +0800

    2020-11-01作业 企业微信app实战-删除联系人PO封装-搜索结果列表页

---
+++ Homework/App11/deleteContact_PO/page/search_page.py
@@ -0,0 +1,17 @@
+from appium.webdriver.common.mobileby import MobileBy
+
+from PythonCode.App11.deleteContact_PO.page.base_page import BasePage
+
+
+class SearchPage(BasePage):
+    # def __init__(self, driver):
+    #     self.driver = driver
+
+    def click_membername(self, name):
+        self.find(MobileBy.XPATH, f'//*[@text="{name}" and @clickable = "false"]').click()
+        from PythonCode.App11.deleteContact_PO.page.briefinfoprofile_page import BriefInfoProfilePage
+        return BriefInfoProfilePage(self.driver)
+
+    def elementexist(self):
+        result = self.find(MobileBy.XPATH, '//*[@text = "无搜索结果"]').text
+        return result
