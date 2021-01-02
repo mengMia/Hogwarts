84ae9d90d1ef512b087491032366bc656ddebabb
mengMia <mengya@ahu.edu.cn>
2021-01-02 15:50:18 +0800

    2020-11-01作业 企业微信app实战-删除联系人PO封装-测试类

--- Homework/App11/deleteContact_PO/testcase/test_deleteContact.py
+++ Homework/App11/deleteContact_PO/testcase/test_deleteContact.py
@@ -1,6 +1,6 @@
 from appium import webdriver
 
-from PythonCode.App11.deleteContact_PO.page.app import App
+from Homework.App11.deleteContact_PO.page.app import App
 
 
 class TestDeleteContact:
@@ -9,6 +9,6 @@ class TestDeleteContact:
         self.main = self.app.start().goto_main()
 
     def test_deleteContact(self):
-        name = "测试21"
+        name = "测试22"
         result = self.main.goto_address().search(name).click_membername(name).click_setting().edit_member().delete_contact().elementexist()
         assert "无搜索结果" == result
