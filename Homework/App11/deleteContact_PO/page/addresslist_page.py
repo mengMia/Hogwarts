449da879cf950fb70a62dda5e4ba21c8afedf9f9
mengMia <mengya@ahu.edu.cn>
2021-01-02 15:48:12 +0800

    2020-11-01作业 企业微信app实战-删除联系人PO封装-通讯录列表页

--- Homework/App11/deleteContact_PO/page/addresslist_page.py
+++ Homework/App11/deleteContact_PO/page/addresslist_page.py
@@ -1,7 +1,7 @@
 from appium.webdriver.common.mobileby import MobileBy
 
-from PythonCode.App11.deleteContact_PO.page.base_page import BasePage
-from PythonCode.App11.deleteContact_PO.page.search_page import SearchPage
+from Homework.App11.deleteContact_PO.page.base_page import BasePage
+from Homework.App11.deleteContact_PO.page.search_page import SearchPage
 
 
 class AddressListPage(BasePage):
