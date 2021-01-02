bc24d815e73d8c119fea9589fce10f6e140d8c67
mengMia <mengya@ahu.edu.cn>
2021-01-02 15:20:50 +0800

    11.6 企业微信app实战-作业-删除联系人PO-编辑联系人页面（删除成员）

---
+++ PythonCode/App11/deleteContact_PO/page/contactedit_page.py
@@ -0,0 +1,14 @@
+from appium.webdriver.common.mobileby import MobileBy
+
+from PythonCode.App11.deleteContact_PO.page.base_page import BasePage
+
+
+class ContactEditPage(BasePage):
+    # def __init__(self, driver):
+    #     self.driver = driver
+
+    def delete_contact(self):
+        self.find(MobileBy.XPATH, '//*[@text = "删除成员"]').click()
+        self.find(MobileBy.XPATH, '//*[@text = "确定"]').click()
+        from PythonCode.App11.deleteContact_PO.page.search_page import SearchPage
+        return SearchPage(self.driver)
