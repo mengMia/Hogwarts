21e58f25f20465c2b6ea72905f08c317314b4c38
mengMia <mengya@ahu.edu.cn>
2021-01-04 20:47:12 +0800

    18.2 接口请求构造-requests框架-构造get和post请求

--- PythonCode/api18/test_api1.py
+++ PythonCode/api18/test_api1.py
@@ -6,3 +6,24 @@ class TestDemo:
         print(r)
         print(r.json())
         assert r.status_code == 200
+
+    # 构造get query请求
+    def test_query(self):
+        payload={
+            "level":1,
+            "name": "ET"
+        }
+        r = requests.get('http://httpbin.testing-studio.com/get', params=payload)
+        print(r.text)
+        assert r.status_code == 200
+
+    # 构造post的form请求，参数应为data
+    def test_post_form(self):
+        payload = {
+            "level": 1,
+            "name": "ET"
+        }
+        r = requests.post('http://httpbin.testing-studio.com/post', data=payload)
+        print(r.text)
+        assert r.status_code == 200
+
