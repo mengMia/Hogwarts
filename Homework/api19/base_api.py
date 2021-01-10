346e665bbcb324e27c6b5663c8a68b3210c5577d
mengMia <mengya@ahu.edu.cn>
2021-01-10 10:58:23 +0800

    2020-12-06作业-企业微信接口实战-接口api object，封装BaseApi和数据清洗

---
+++ Homework/api19/base_api.py
@@ -0,0 +1,37 @@
+import json
+
+import requests
+
+
+class BaseApi:
+    def __init__(self):
+        self.token = self.get_token()
+
+    def get_token(self):
+        corpid = 'ww485f3e5163864f5a'
+        corpsecret = 'orTmGl1LvfG_7MNsnTDaH5MUAdy1rgiaiPz5hkaxVxI'
+        data = {
+            "method": "get",
+            "url": "https://qyapi.weixin.qq.com/cgi-bin/gettoken",
+            "params": {'corpid': corpid, 'corpsecret': corpsecret}
+        }
+        r = self.send(data)
+        # print(json.dumps(r.json(), indent=2))
+        # 断言可以加也可以不加，但是如要测获取token失败的情况，就不要加断言了，加了断言会在这里报错，后续的用例就不执行了？
+        # 如果是说这里出错后面的用例就不需要再跑了,那么可以加断言
+        assert r.status_code == 200
+        assert r.json()['errcode'] == 0
+        print("token获取")
+        token = r.json()['access_token']
+        return token
+
+    def send(self, kwargs):
+        """
+        封装request方法
+        :param kwargs:
+        :return:
+        """
+        # request方法是get和post的底层方法，只要传入请求方式就可以了
+        r = requests.request(**kwargs)
+        print(json.dumps(r.json(), indent=2))
+        return r
--- Homework/api19/tag.py
+++ Homework/api19/tag.py
@@ -3,63 +3,97 @@ import json
 
 import requests
 
+from PythonCode.api19.base_api import BaseApi
 
-class Tag:
-    # 1.token可以放到类里面定义,当token修改的时候变成对类的修改？？？
-    # token=None
-    # 2.token可以放在init方法里，创建对象的时候就初始化好，只需要在初始化的时候获取一次就可以，在test_tag里获取token，只需要实例化这个类就可以获取到token：tag=Tag()
+
+class Tag(BaseApi):
     def __init__(self):
-        self.token=self.get_token()
+        """
+        继承父类的init方法，获取token
+        """
+        super().__init__()
 
-    # 获取token理论上来说不应该放在这个地方，应该放在一个公共的地方
-    def get_token(self):
-        corpid = 'ww485f3e5163864f5a'
-        corpsecret = 'orTmGl1LvfG_7MNsnTDaH5MUAdy1rgiaiPz5hkaxVxI'
-        r = requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken",
-                         params={'corpid': corpid, 'corpsecret': corpsecret})
-        print(json.dumps(r.json(), indent=2))
-        # 断言可以加也可以不加，但是如要测获取token失败的情况，就不要加断言了，加了断言会在这里报错，后续的用例就不执行了？
-        # 如果是说这里出错后面的用例就不需要再跑了,那么可以加断言
-        assert r.status_code == 200
-        assert r.json()['errcode'] == 0
-        token = r.json()['access_token']
-        return token
+    def find_group_id_by_name(self, group_name):
+        """
+        判断group_name是否已经存在
+        :param group_name:
+        :return:
+        """
+        for group in self.list().json()["tag_group"]:
+            if group_name in group["group_name"]:
+                return group["group_id"]
+        print("group name not in goup")
+        return ""
 
+    def is_group_id_exist(self, group_id):
+        """
+        判断group_id是否已经存在
+        :param group_id:
+        :return:
+        """
+        for group in self.list().json()["tag_group"]:
+            if group_id in group["group_id"]:
+                print("这个是is_group_id_exist方法，return true")
+                return True
+        print("这个是is_group_id_exist方法，return false")
+        return False
 
-    def add(self, group_name, tag):
+    def add(self, group_name, tag, **kwargs):
         """
         添加标签
         :param group_name:
         :param tag:
         :return:
         """
-        r = requests.post("https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag",
-                          params={"access_token": self.token},
-                          json={
-                                "group_name": group_name,
-                                "tag": tag
-                            }
-                          )
-        print(json.dumps(r.json(), indent=2))
+        data = {
+                "method": "post",
+                "url": "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag",
+                "params":{"access_token": self.token},
+                "json":{
+                        "group_name": group_name,
+                        "tag": tag,
+                        **kwargs
+                        }
+                }
+        r = self.send(data)
         return r
+
+    def add_and_detect(self, group_name, tag, **kwargs):
+        """
+        解决添加标签的时候标签重复导致执行失败的问题, 数据清理
+        :param group_name:
+        :param tag:
+        :param kwargs:
+        :return:
+        """
+        r = self.add(group_name, tag, **kwargs)
+        # 如果请求状态码正常，接口的错误码是40071，说明已经有group_name存在，要先检验是不是真的存在于现有的tag列表中，检验方法里返回了group_id，然后再删掉这个group_id
+        if r.json()["errcode"] == 40071:
+            group_id = self.find_group_id_by_name(group_name)
+            if not group_id:
+                # 这个返回说明是接口有问题
+                return ""
+            self.delete_group([group_id])
+            self.add(group_name, tag, **kwargs)
+        result = self.find_group_id_by_name(group_name)
+        if not result:
+            print("add not success")
+        return result
 
     def list(self):
         """
         获取标签列表
         :return: 列表r
         """
-        r = requests.post(
-            # post请求带参数的话，放在params里
-            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list?',
-            params={"access_token": self.token},
-            json={
+        data = {
+            "method": "post",
+            "url": "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list?",
+            "params": {"access_token": self.token},
+            "json":{
                 "tag_id": []
             }
-        )
-        print(json.dumps(r.json(), indent=2))
-        # list也要测试某个查询失败的情况，所以去掉断言
-        # assert r.status_code == 200
-        # assert r.json()['errcode'] == 0
+        }
+        r = self.send(data)
         return r
 
     def update(self, id, tag_name):
@@ -67,30 +101,67 @@ class Tag:
         编辑标签
         :return:编辑标签接口的响应json数据
         """
-        r = requests.post("https://qyapi.weixin.qq.com/cgi-bin/externalcontact/edit_corp_tag",
-                          params={"access_token": self.token},
-                          json={
-                              "id": id,
-                              "name": tag_name
-                          })
-        print(json.dumps(r.json(), indent=2))
+        data = {
+            "method": "post",
+            "url": "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/edit_corp_tag",
+            "params": {"access_token": self.token},
+            "json": {
+                "id": id,
+                "name": tag_name
+                }
+        }
+        r = self.send(data)
         return r
 
-    def delete(self, tag_id, group_id):
+    def delete_group(self, group_id):
         """
-        删除标签
-        :param tag_id:
+        根据group_id删除标签
         :param group_id:
         :return:
         """
+        data = {
+            "method": "post",
+            "url": "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag",
+            "params": {"access_token": self.token},
+            "json": {
+                "group_id": group_id
+            }
+        }
+        r = self.send(data)
+        return r
 
-        r = requests.post("https://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag",
-                          params={"access_token": self.token},
-                          json={
-                              "tag_id": tag_id,
-                              "group_id": group_id
-                          })
-        print(json.dumps(r.json(), indent=2))
+    def delete_tag(self, tag_id):
+        """
+        根据tag_id删除标签
+        :param tag_id:
+        :return:
+        """
+        data = {
+            "method": "post",
+            "url": "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag",
+            "params": {"access_token": self.token},
+            "json": {
+                "tag_id": tag_id,
+            }
+        }
+        r = self.send(data)
+        return r
+
+    def delete_and_detect_group(self, group_ids):
+        delete_group_ids = []
+        print("第一次调用delete_group方法")
+        r = self.delete_group(group_ids)
+        # 40068说明这个group id不存在或者不合法，要检测是否真的不存在
+        if r.json()["errcode"] == 40068:
+            for group_id in group_ids:
+                if not self.is_group_id_exist(group_id):
+                    group_id_tmp = self.add_and_detect("123", [{"name": "标签123"}])
+                    delete_group_ids.append(group_id_tmp)
+                else:
+                    delete_group_ids.append(group_id)
+            print("第二次调用delete_group方法")
+            r = self.delete_group(delete_group_ids)
+        print("删除成功")
         return r
 
     def get_tag_id(self, group_name):
