b3429594d321b68210ff85a6745eece1be4eab41
mengMia <mengya@ahu.edu.cn>
2021-01-11 21:09:27 +0800

    12.3+4 UI自动化测试框架封装实战-打造自己的测试框架-雪球主页

--- PythonCode/UI12/frame/main_page.py
+++ PythonCode/UI12/frame/main_page.py
@@ -4,10 +4,10 @@ from PythonCode.UI12.frame.base_page import BasePage
 from PythonCode.UI12.frame.market_page import MarketPage
 
 
-class MainPage:
+class MainPage(BasePage):
     def goto_market(self):
         # 在点击行情tab之前，点击那个编辑按钮，弹出登录页面，制造异常
-        BasePage().find(By.XPATH, "//*[@resource-id='com.xueqiu.android:id/post_status']").click()
+        self.find(By.XPATH, "//*[@resource-id='com.xueqiu.android:id/post_status']").click()
         # 点击行情
-        BasePage().find(By.XPATH, "//*[@resource-id='android:id/tabs']//*[@text='行情']").click()
-        return MarketPage()
+        self.find(By.XPATH, "//*[@resource-id='android:id/tabs']//*[@text='行情']").click()
+        return MarketPage(self.driver)
