a215617b821a858de8ec3ee6b174dec3d531d345
mengMia <mengya@ahu.edu.cn>
2021-01-11 21:09:40 +0800

    12.3+4 UI自动化测试框架封装实战-打造自己的测试框架-雪球APP行情页

--- PythonCode/UI12/frame/market_page.py
+++ PythonCode/UI12/frame/market_page.py
@@ -4,7 +4,7 @@ from PythonCode.UI12.frame.base_page import BasePage
 from PythonCode.UI12.frame.search_page import SearchPage
 
 
-class MarketPage:
+class MarketPage(BasePage):
     def goto_search(self):
-        BasePage().find(By.ID, "com.xueqiu.android:id/action_search").click()
-        return SearchPage()
+        self.find(By.ID, "com.xueqiu.android:id/action_search").click()
+        return SearchPage(self.driver)
